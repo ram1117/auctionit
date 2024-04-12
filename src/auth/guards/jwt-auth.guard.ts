c174c31728ee1260c46940e5ad75bf2d3100396b
ram1117 <ramkumark1@outlook.com>
2024-04-12 17:23:14 +0530

    make jwt auth guard injectable

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,11 +1,12 @@
-import { ExecutionContext, Inject } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 import { IS_PUBLIC_KEY } from '../../decorators/public.decorator';
 import { Observable } from 'rxjs';
 
+@Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
-  constructor(@Inject(Reflector) private reflector: Reflector) {
+  constructor(private reflector: Reflector) {
     super();
   }
 
