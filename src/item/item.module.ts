6601991b34741174f48dad930e46678f76f41c92
ram1117 <ramkumark1@outlook.com>
2024-04-22 01:12:10 +0530

    remove supabase image storage and move to front end

--- src/item/item.module.ts
+++ src/item/item.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ItemController } from './item.controller';
 import { ItemService } from './item.service';
-import { SupabaseModule } from '../supabase/supabase.module';
 
 @Module({
   controllers: [ItemController],
   providers: [ItemService],
-  imports: [SupabaseModule],
 })
 export class ItemModule {}
